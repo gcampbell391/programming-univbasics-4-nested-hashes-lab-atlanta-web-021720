3775a5e8171b002167428f773ef8a8df29805941
Gene Campbell III <gcampbell391@gmail.com>
2020-02-07 02:38:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,8 +60,9 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby",
-  programmer_hash[:yukihiro_matsumoto][:languages] = "LISP" "C"
+  programmer_hash = {:yukihiro_matsumoto => {:known_for => "Ruby",
+                                        :languages => ["LISP", "C"]
+
 end
 	programmer_hash = 
  		{
