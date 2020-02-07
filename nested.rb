824d32ef79bf46b44e0b79391fa3302e4fdcca1d
Gene Campbell III <gcampbell391@gmail.com>
2020-02-07 02:48:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     return programmer_hash[:alan_kay][:known_for]["GUI"] 
+     return programmer_hash[:alan_kay][:known_for] = "GUI"
      
      
 end
