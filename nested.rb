3e9d63ff6a2442e40fbdd9c1b252fe5f6f149afb
Gene Campbell III <gcampbell391@gmail.com>
2020-02-07 02:45:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,25 +65,6 @@ def adding_matz
                                         }
   }
 end
-def changing_alan
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
-    
-end
 
 def changing_alan
 	programmer_hash = 
